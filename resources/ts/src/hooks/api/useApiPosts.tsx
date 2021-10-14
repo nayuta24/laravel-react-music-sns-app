6315debff606705e06c3759a778190c092098bfb
NMYuta <yutanakamu824@gmail.com>
2021-10-14 11:08:40 +0900

    最新の投稿ページでlaravelのapiから情報を取得できるようにした

--- resources/ts/src/hooks/api/useApiPosts.tsx
+++ resources/ts/src/hooks/api/useApiPosts.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { PostsDataType } from "../../type/api/PostsDataType";
 
 export const useApiPosts = () => {
-  const [api_posts, setApiPosts] = useState<Array<PostsDataType>>([]);
-  axios
-    .get<Array<PostsDataType>>("/api/posts")
-    .then((res) => setApiPosts(res.data));
-  return { api_posts };
+    const [api_posts, setApiPosts] = useState<Array<PostsDataType>>([]);
+    axios
+        .get<Array<PostsDataType>>("/api/posts")
+        .then((res) => setApiPosts(res.data));
+    return { api_posts };
 };
