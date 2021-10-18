9ba1c052fc8e2a9ccd03c49997ae22bebe68e367
nayuta24 <yutanakamu824@gmail.com>
2021-10-18 18:01:57 +0900

    投稿詳細画面apiを作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -57,7 +57,37 @@ class PostController extends Controller
         return $posts_json;
     }
     
-    public function detail(Post $post){
+    public function detail(Post $post, $id){
+        $post_detail = Post::find($id);
+        $post_user = User::find($post_detail->user_id);
         
+        return [
+                "id"=>$post_detail->id,
+                "created_at"=>$post_detail->created_at,
+                "updated_at"=>$post_detail->updated_at,
+                "user" => [
+                    "id"=>$post_user->id,
+                    "img"=>$post_user->user_img,
+                    "name"=>$post_user->name,
+                    "job"=>$post_user->job,
+                ],
+                "track" => [
+                    "id"=>"5ViZUsnQEwWw2bdFuW78y8",
+                    "img"=>"https://i.scdn.co/image/ab67616d00001e029259361b006ad3108801a541",
+                    "artist"=>"キリンジ",
+                    "title"=>"時間がない",
+                    "album"=>"愛があるだけ、すべて",
+                    "release"=>"2019/10/18"
+                ],
+                "review" => [
+                    "star"=>$post_detail->rate,
+                    "title"=>$post_detail->title,
+                    "body"=>$post_detail->body
+                ],
+                "reaction" => [
+                    "comment_val"=>0,
+                    "like_val"=>11
+                ]
+            ];
     }
 }
