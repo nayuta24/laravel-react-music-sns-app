2f856a9e9687b4806d6b7234aa61ea0f36c7d2f1
NMYuta <yutanakamu824@gmail.com>
2021-10-19 21:16:12 +0900

    投稿詳細画面の戻るボタンのアイコンを変更

--- resources/ts/src/components/atoms/button/HamburgerButton.tsx
+++ resources/ts/src/components/atoms/button/HamburgerButton.tsx
@@ -3,26 +3,26 @@ import { HamburgerIcon } from "@chakra-ui/icons";
 import { memo, VFC } from "react";
 
 type Props = {
-  display?: {};
-  onClick: () => void;
-  size?: string;
-  ml?: string;
-  my?: string;
+    display?: {};
+    onClick: () => void;
+    size?: string;
+    ml?: string;
+    my?: string;
 };
 
 export const HamburgerButton: VFC<Props> = memo((props) => {
-  const { display, onClick, ml = "none", my = "none", size = "md" } = props;
-  return (
-    <IconButton
-      bgGradient="linear(to-tl, green, #c4de71)"
-      color="white"
-      aria-label="Open menu"
-      icon={<HamburgerIcon />}
-      size={size}
-      ml={ml}
-      my={my}
-      display={display}
-      onClick={onClick}
-    />
-  );
+    const { display, onClick, ml = "none", my = "none", size = "md" } = props;
+    return (
+        <IconButton
+            bgGradient="linear(to-tl, green, #c4de71)"
+            color="white"
+            aria-label="Open menu"
+            icon={<HamburgerIcon />}
+            size={size}
+            ml={ml}
+            my={my}
+            display={display}
+            onClick={onClick}
+        />
+    );
 });
