04a1621cca87b743376ba540c9e328ec9fae970d
nayuta24 <yutanakamu824@gmail.com>
2021-10-23 00:32:15 +0900

    [add]sanctum middlewareに投稿一覧apiなどを入れ、ログインしてないとエラーメッセージが返されるようにした

--- routes/api.php
+++ routes/api.php
@@ -20,17 +20,23 @@ use Illuminate\Http\Request;
 // ログインしているユーザーのみアクセスできる
 // ログインしていない場合はエラーが吐かれてログインページにリダイレクトされる
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    // ログインしてるユーザーの情報をjsonで返す
     Route::get('/me', MeController::class);
+    
+    // 投稿一覧を最新順に返す
+    Route::get('/posts/recent', 'PostController@posts');
+
+    // 投稿詳細情報をidごとに返す
+    Route::get('/posts/{id}', 'PostController@detail');
 });
 
+// 新規ユーザー登録
 Route::post('/register', 'RegisterUserController@register');
 
-Route::get('/posts/recent', 'PostController@posts');
-
-Route::get('/posts/{id}', 'PostController@detail');
-
+// ログインする
 Route::post('/login', 'MeController@login');
 
+// ログアウトする
 Route::post('/logout', 'MeController@logout');
 
 // Route::get('/users/me', 'UserController@show')->name('user');
