5a6e0dbf6cf392010bdfcbcb13900343f5ef058c
NMYuta <yutanakamu824@gmail.com>
2021-11-07 22:35:09 +0900

    [fix]ProfileModalのボタン表示関連フラグの初期値をなくす

--- resources/ts/src/router/Router.tsx
+++ resources/ts/src/router/Router.tsx
@@ -17,28 +17,28 @@ export const Router = () => {
                 {isLogin ? <Redirect to="/" /> : <Login />}
             </Route>
             {/* 未ログインの場合は「AuthGuard」によってログイン画面へリダイレクトされる */}
-            <AuthGuard>
-                <BaseLayout>
-                    <Switch>
-                        <Route
-                            path="/"
-                            render={({ match: { url } }) => (
-                                <Switch>
-                                    {homeRoutes.map((route) => (
-                                        <Route
-                                            key={route.path}
-                                            exact={route.exact}
-                                            path={`${url}${route.path}`}
-                                        >
-                                            {route.children}
-                                        </Route>
-                                    ))}
-                                </Switch>
-                            )}
-                        />
-                    </Switch>
-                </BaseLayout>
-            </AuthGuard>
+            {/* <AuthGuard> */}
+            <BaseLayout>
+                <Switch>
+                    <Route
+                        path="/"
+                        render={({ match: { url } }) => (
+                            <Switch>
+                                {homeRoutes.map((route) => (
+                                    <Route
+                                        key={route.path}
+                                        exact={route.exact}
+                                        path={`${url}${route.path}`}
+                                    >
+                                        {route.children}
+                                    </Route>
+                                ))}
+                            </Switch>
+                        )}
+                    />
+                </Switch>
+            </BaseLayout>
+            {/* </AuthGuard> */}
             <Route path="*">
                 <Page404 />
             </Route>
